a4a8f5adbd5940100f75ba21a1c6f73d19ae8edc
misio <kartofel6223@gmail.com>
2022-08-03 22:52:26 +0200

    xd

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -150,11 +150,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
